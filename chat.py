67f5c96a561a9ac4d1b14dd073700301dd5936d3
jenngold20 <jennifer.goldfeld@usal.edu.ar>
2024-01-21 15:11:00 -0300

    Api key

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "..."
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
